b3bd3b86baf1b2250a95e1b2530c431c529b252d
jp9506 <jp9506@gmail.com>
2019-06-11 06:46:41 -0400

    Adding test program
    Initial stab at dll imports

---
+++ mpir.core/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using mpir.core;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mpz_t one = new mpz_t(1);
+            Console.WriteLine((int)one);
+            //Console.WriteLine(one.ToString());
+        }
+    }
+}
---
+++ mpir.core/build.vc19/mpir.core/mpz_t.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mpir.core
+{
+    public class mpz_t
+    {
+        private IntPtr me = new IntPtr();
+
+        public mpz_t() { internals.mpir.__gmpz_init(ref me); }
+        public mpz_t(int x) { internals.mpir.__gmpz_init_set_si(ref me, x); }
+        public mpz_t(uint x) { internals.mpir.__gmpz_init_set_ui(ref me, x); }
+        public mpz_t(long x) { internals.mpir.__gmpz_init_set_sx(ref me, x); }
+        public mpz_t(ulong x) { internals.mpir.__gmpz_init_set_ux(ref me, x); }
+        public mpz_t(string x) { internals.mpir.__gmpz_init_set_str(ref me, x, 10); }
+
+        ~mpz_t() { internals.mpir.__gmpz_clear(ref me); }
+
+        public override string ToString()
+        {
+            var res = "";
+            //internals.mpir.__gmpz_get_str(ref res, 10, ref me);
+            return res;
+        }
+
+        public static mpz_t operator +(mpz_t left, mpz_t right)
+        {
+            var res = new mpz_t();
+            internals.mpir.__gmpz_add(ref res.me, ref left.me, ref right.me);
+            return res;
+        }
+
+        public static explicit operator int(mpz_t x)
+        {
+            int res;
+            internals.mpir.__gmpz_get_si(out res, ref x.me);
+            return res;
+        }
+    }
+}
